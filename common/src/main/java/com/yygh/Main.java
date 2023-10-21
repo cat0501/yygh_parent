a5370542e37db04c0f65e29e1980752811185481
Lemonade19 <pxzjl123@icloud.com>
2023-10-21 11:14:55 +0800

    初始环境搭建

---
+++ common/src/main/java/com/yygh/Main.java
@@ -0,0 +1,12 @@
+package com.yygh;
+
+/**
+ * @author cat
+ * @description
+ * @date 2023/10/21 11:03
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/yygh/Main.java
@@ -0,0 +1,12 @@
+package com.yygh;
+
+/**
+ * @author cat
+ * @description
+ * @date 2023/10/21 10:55
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
